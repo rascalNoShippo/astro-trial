054aa5fe4df8225c1dbff0687f80fda81c0bcd67
kurosawanao <kurosawanao@team-lab.com>
2024-04-18 20:44:14 +0900

    export修正

---
+++ src/pages/_index.ts
@@ -0,0 +1,9 @@
+export const formatter = new Intl.DateTimeFormat("ja", {
+	year: "numeric",
+	month: "long",
+	day: "numeric",
+	weekday: "short",
+	hour: "numeric",
+	minute: "numeric",
+	second: "numeric",
+});
