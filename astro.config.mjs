f1b469e201b96598a8494932fd226fb8236c1aba
kurosawanao <kurosawanao@team-lab.com>
2024-06-11 15:30:13 +0900

    chore: configテスト

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,5 +13,13 @@ export default defineConfig({
       }
     }
   },
-  integrations: [react()]
+  integrations: [react()],
+  server: {
+    headers: {
+      "x-frame-options": "DENY",
+      "x-xss-protection": "1; mode=block",
+      "x-content-type-options": "nosniff",
+      "content-security-policy": "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; media-src 'self'; object-src 'none'; frame-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; manifest-src 'self'; worker-src 'self'; upgrade-insecure-requests; block-all-mixed-content; disown-opener; sandbox allow-forms allow-same-origin allow-scripts; reflected-xss block; referrer no-referrer; report-to csp-endpoint;"
+    }
+  }
 });
