34748934db9dfc5b027c7bb0655880c0678ea997
kurosawanao <kurosawanao@team-lab.com>
2024-06-11 16:48:42 +0900

    output: static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import { defineConfig } from 'astro/config';
+
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -8,21 +8,18 @@ export default defineConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@use 'src/styles/_variables' as *;`,
-        },
-      },
-    },
+          additionalData: `@use 'src/styles/_variables' as *;`
+        }
+      }
+    }
   },
-  output: "server",
-  adapter: node({ mode: "standalone" }),
   integrations: [react()],
   server: {
     headers: {
       "x-frame-options": "DENY",
       "x-xss-protection": "1; mode=block",
       "x-content-type-options": "nosniff",
-      "content-security-policy":
-        "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; media-src 'self'; object-src 'none'; frame-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; manifest-src 'self'; worker-src 'self'; upgrade-insecure-requests; block-all-mixed-content; disown-opener; sandbox allow-forms allow-same-origin allow-scripts; reflected-xss block; referrer no-referrer; report-to csp-endpoint;",
-    },
-  },
+      "content-security-policy": "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; media-src 'self'; object-src 'none'; frame-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; manifest-src 'self'; worker-src 'self'; upgrade-insecure-requests; block-all-mixed-content; disown-opener; sandbox allow-forms allow-same-origin allow-scripts; reflected-xss block; referrer no-referrer; report-to csp-endpoint;"
+    }
+  }
 });
